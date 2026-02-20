dd4a926d856a1f4da30d7a7709fa7b0d84ac6532
dishitas8 <dishita160306@gmail.com>
2026-02-20 10:40:13 +0530

    Index

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-//console.log("Hello World");
-document.write("Hello World");
+// // console.log("Hello World");
+// // document.write("Hello World");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="sanjeev soni";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined; 
+// var sayHello=function(){
+//     alert("Hello World");
+// }
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1===sym2){
+console.log("true");
+}
+else {
+    console.log("false");
+}
+var a=100; var b=13; var c=10; var linebreak="<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
