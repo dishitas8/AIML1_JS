8757eb8408290b87b0f9cf9b5113c306e390ef5a
dishitas8 <dishita160306@gmail.com>
2026-03-20 09:39:19 +0530

    DOM Method (JS)

--- dommethods.js
+++ dommethods.js
@@ -1,2 +1,8 @@
 const heading=document.createElement("h2");
-const bodytag=document.getElementsByTagName("body");
+const bodytag=document.getElementsByTagName("body");        
+function 
+onhandlefocus() {
+    XMLDocument.style.background="red";
+}  
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
